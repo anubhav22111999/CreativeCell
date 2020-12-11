fe7d56dcc3abe5d4833f2b0fbb21962482105402
anubhav22111999 <anubhav.1721cs1161@kiet.edu>
2020-12-11 21:03:21 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    
+    
+    
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Creative cell</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+
+
+  
+<script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+
+<!-- for minor updates and patch fixes within a major version -->
+
+<script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@14/dist/smooth-scroll.polyfills.min.js"></script>
+
+<!-- for patch fixes within a minor version -->
+
+<script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@14.0/dist/smooth-scroll.polyfills.min.js"></script>
+
+<!-- for a specific version -->
+
+<script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@14.0.0/dist/smooth-scroll.polyfills.min.js"></script>
+</head>
+<body>
+  <?php
+   echo “This is PHP code”;
+   ?>
+    <header> 
+        <h2> <a href="#"> <img src="imgs/ccell_logo.png" height="80px" width="80px"> </a></h2>
+        <nav>
+        <li><a href="#projects" style="color: black;"> Projects </a></li>
+        <li><a href="#technologies"style="color: black;"> Domains of Work </a></li>
+        <li><a href="#contact"style="color: black;"> Contact Us </a></li>
+        </nav>
+    </header>
+
+    <!-- main headder -->
+
+    <section class="hero">
+        <div class="background"></div>
+        <div class="content">
+        <h1>Creative Cell</h1>    
+         <div class="bounce">
+        <p>" Don't Dump your ideas " </p>
+        </div>
+        <a class="btn" href="#about_us"> About Us . . . </a>
+        </div>
+    </section>
+
+
+<!--team section details -->
+
+<section>
+            <h1>Meet Team</h1>
+            <p align="right ">Great things are done by a series of small things brought together.</p>
+        <div class="row">
+          
+
+          <div class="col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="">
+            <div class="team-member">
+              <figure>
+                <ul class="social">
+                  <li><a href="#"><i class="fab fa-twitter-square fa-2x"></i></a></li>
+                  <li><a href="#"><i class="fab fa-facebook-square fa-2x"></i></a></li>
+                  <li><a href="#"><i class="fab fa-linkedin fa-2x"></i></a></li>
+                </ul>
+                <img src="imgs/person_2.jpg" alt="Image" class="img-fluid" height="300px" width="300px" style="vertical-align: left;">
+              </figure>
+              <div class="p-3">
+                <h3>Mr. Vinay Ahlawat</h3>
+                <span class="position">Manager ITSS</span>
+              </div>
+            </div>
+          </div>
+
+         
+          <div class="col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="team-member">
+              <figure>
+                <ul class="social">
+                  <li><a href="#"><i class="fab fa-twitter-square fa-2x"></i></a></li>
+                  <li><a href="#"><i class="fab fa-facebook-square fa-2x"></i></a></li>
+                  <li><a href="#"><i class="fab fa-linkedin fa-2x"></i></a></li>
+                </ul>
+                <img src="imgs/person_3.jpeg" alt="Image" class="img-fluid" height="300px" width="300px">
+              </figure>
+              <div class="p-3">
+                <h3>Mr. Prateek Kumar</h3>
+                <span class="position">Manager</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="300">
+            <div class="team-member">
+              <figure>
+                <ul class="social">
+                  <li><a href="#"><i class="fab fa-twitter-square fa-2x"></i></a></li>
+                  <li><a href="#"><i class="fab fa-facebook-square fa-2x"></i></a></li>
+                  <li><a href="#"><i class="fab fa-linkedin fa-2x"></i></a></li>
+                </ul>
+                <img src="imgs/person_5.png" alt="Image" class="img-fluid" height="300px" width="300px">
+              </figure>
+              <div class="p-3">
+                <h3>Anubhav Gautam</h3>
+                <span class="position">Accountant</span>
+              </div>
+            </div>
+          </div>
+
+         
+        </div>
+      </div>
+    </section>
+
+    <!-- About us information -->
+
+
+    <section class="about_us" id="about_us" style="background-color: rgba(157,204,230,0.5);">
+      <h1 style="font-size: 40px;">About Us.</h1><hr style="color: black;">
+        <ul>
+            <li style="list-style:disc;">Promote the creativity and talent among our students and faculty members<br></li>
+            <li style="list-style:disc;">Encourage students, talented in painting, graphic designing, photography,
+                videography , acting and other to utilize their potentials.<br></li>
+            <li style="list-style:disc;">Bring together the students and faculty members who can contribute in promoting
+                branding activities of KIET Group of Institutions through our Facebook page, You
+                Tube Channel, Magazine, Brochure and others.<br></li>
+            <li style="list-style:disc;">Gather the volunteers who can assist in showcasing the activities for all the events
+                organized in the campus including seminars, conferences, frescos, tech fest,
+                cultural fest, epoque etc.<br></li>
+          </ul>
+    </section>
+    
+<!-- domains details -->
+        
+          <section class="packages" id="technologies">
+            <h3 class="title">Our Field Of works</h3>
+            <p>We offer a variety of areas to works and enhance your skills. Whether you've climbed Everest or don't even know what a mountain is, we welcome every interested and hardworking buddy.</p>
+            <hr>
+        
+            <ul class="grid">
+              <li>
+                <i ><img src="imgs/graphics.png" height="200px" width="300px"></i>
+                <h4>Graphic Design</h4>
+                <p> Graphic design is the process of <b>visual communication </b>and problem-solving through the use of <b>typography, photography and illustration</b>.
+                <p>We create and combine symbols, images and text to form visual representations of ideas and messages. We use typography, visual arts and page layout techniques to create visual compositions.
+                </p>
+                </p>
+              </li>
+              <li>
+                <i><img src="imgs/pnv.png" height="200px" width="300px"></i>
+                <h4>Photography & Videography</h4>
+                <p> We cover all the major fests organized in college and release the aftermovies for events like <b>Prastuti, Epoque, Innotech </b> etc.
+                </p>
+                <p>
+                  We also create short movies based on social issues as well as we do photoshoots on creative and burning topics.
+s                </p>
+
+              </li>
+              <li>
+                <i><img src="imgs/content.png" height="200px" width="325px"></i>
+                <h4>Content Writing</h4>
+                <p> We write all <b>formal and informal content</b>
+                    required at various <b>social media
+                    platforms</b>.
+
+                    Writing our personal stuff as in poetries 
+                    and also giving formal captions for
+                    various posts.
+
+                    Learning to write according to what we
+                    want to. Hence enhancing our skill set.
+                </p>
+                <p>
+                  We manage the <b>official KIET
+                  Facebook and Instagram
+                  handle</b>.
+
+                  Helping in spreading
+                  information to larger audience
+                  and keeping everyone in
+                  touch.
+
+                </p>
+              </li>
+              <li>
+                <i><img src="imgs/creativearts.png" height="200px" width="300px"></i>
+                <h4>Creative Arts</h4>
+                <p>Art is a finest  platform of expressing our thoughts and imagination." 
+                   Being imaginative and being creative is two different things . If you  convey your idea into a different style ,then that is creativity. 
+                   We Creative arts team works for the college <b>cultural and technical fest </b>where are job is to decorate the whole corner of our college by creating aur own original artwork. Our team also  do <b>paintings, sketching , crafting , wall art etc</b>.</p>
+                
+ </p>
+              </li>
+            </ul>
+          </section>
+
+
+<!-- project details -->
+
+          <section class="destinations" id="projects">
+            <h3 class="title">Some of our Projects </h3>
+            <p> we run different project that keeps people aware of various social issues around the world. we provide a platform to other willing students to put forward their hidden talent through our different projects. </p>
+            <hr>
+        
+            <ul class="grid">
+              <li class="small" style="background-image: url(imgs/project1.jpg); font-size: 12px;"><a href="https://www.youtube.com/watch?v=V5ElzEvzKoI"><i>Explore more:</i></a></li>
+              <li class="large" style="background-image: url(imgs/project2.jpg); font-size: 12px;"><a href="https://www.youtube.com/watch?v=Z-yn_m-KbvE"><i>Explore more:</i></a></li>
+              <li class="large" style="background-image: url(imgs/project3.jpg); font-size: 12px;"><a href="https://www.youtube.com/watch?v=1g8yqaK_TzE"><i>Explore more:</i></a></li>
+              <li class="small" style="background-image: url(imgs/project4.jpg); font-size: 12px;"><a href="https://www.youtube.com/watch?v=TRnA5kg1t3U"><i>Explore more:</i></a></li>
+            </ul>
+          </section>
+
+
+<!-- customer reviews -->
+
+          <section class="testimonials">
+                <h3 class="title">Reviews</h3>
+                <hr>
+                <p class="quote">Vsery nice platform to groom your skills in your hobbies along with academic </p>
+                <p class="author">- Anubhav gautam</p>
+                <p class="quote">Good team support thought me a lot about how to manage time and how to be more expressive.</p>
+                <p class="author">- Aman</p>
+                <p class="quote">Help me to get my dream job of an graphic designer.</p>
+                <p class="author">- Priyanshu Gupta</p>
+              </section>
+            
+              <section class="contact">
+                <h3 class="title">Get in Touch:</h3>
+                <p> " Their is no greater agony than bearing an untold story inside you. "  </p>
+                <hr>
+                <form>
+                  <input type="email" placeholder="Email">
+                  <a href="#" class="btn">Subscribe now</a>
+                </form>
+              </section>
+              
+  <!-- contact details -->
+  
+
+              <footer id="contact">
+                <p>KIET Groups Of Institution</p><br><br>
+                <p>© Copyright All Rights Reserved     Shivendra_shrivastva</p>
+                <ul>
+                  <li><a href="#"><i class="fab fa-twitter-square fa-2x"></i></a></li>
+                  <li><a href="#"><i class="fab fa-facebook-square fa-2x"></i></a></li>
+                  <li><a href="#"><i class="fab fa-linkedin fa-2x"></i></a></li>
+                </ul>
+              </footer>
+
+
+              <script>
+                    var scroll = new SmoothScroll('a[href*="#contact"]' , { 
+                        speed: 2000 , 
+                    });
+                    var scroll = new SmoothScroll('a[href*="#projects"]' , { 
+                        speed: 2000 , 
+                    });
+                    var scroll = new SmoothScroll('a[href*="#technologies"]' , { 
+                        speed: 2000 , 
+                    });
+                </script>
+
+</body>
+</html>
